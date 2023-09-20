db4be38205ceaa6b41293348b43f489911c8c190
DESKTOP-VGH69S8\AlexeyShakov <alexei_96@inbox.ru>
2023-09-20 20:29:28 +0300

    created table for errors when processing Post

---
+++ enums.py
@@ -0,0 +1,6 @@
+import enum
+
+
+class StepNameChoice(enum.Enum):
+    TRANSTALTION = "translation"
+    TELEGRAM = "telegram"
--- models.py
+++ models.py
@@ -1,10 +1,31 @@
+from sqlalchemy.orm import relationship, backref
+
 from db_connection import Base
-from sqlalchemy import Integer, Column, Text
+from sqlalchemy import Integer, Column, Text, Enum, ForeignKey
+
+from enums import StepNameChoice
+
+class Error(Base):
+    """
+    Описывает, на каком этапе произошла ошибка при обработке новости
+    """
+    __tablename__ = "errors"
+
+    id = Column(Integer, primary_key=True)
+    step = Column(Enum(StepNameChoice), nullable=False, unique=True)
+
+    posts = relationship("Post", back_populates="error")
 
 class Post(Base):
+    """
+    Описывает новости на сайте
+    """
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True)
     link = Column(Text, nullable=False)
     title = Column(Text, nullable=False)
     short_description = Column(Text, nullable=False)
+    error_id = Column(Integer, ForeignKey('errors.id'), nullable=True)
+
+    error = relationship("Error", back_populates="posts", lazy="joined")
